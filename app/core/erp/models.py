6182a7bf7ab551b507044b7c779371064ae56b50
Francisco Chavarría <102611026+francisch-98@users.noreply.github.com>
2022-10-04 10:42:50 -0600

    update to models.py and add choices.py

--- app/core/erp/models.py
+++ app/core/erp/models.py
@@ -1,53 +1,48 @@
 from django.db import models
-from django.utils import timezone
+from datetime import datetime
 
-
-class Type(models.Model):
-    name = models.CharField(max_length=150, verbose_name='Nombre', unique=True)
-
-    def __str__(self):
-        return self.name
-
-    class Meta:
-        verbose_name = 'Tipo'
-        verbose_name_plural = 'Tipos'
-        db_table = 'tipo'
-        ordering = ['id']
+from core.erp.choices import gender_choices
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=150, verbose_name='Nombre')
+    name = models.CharField(max_length=150, verbose_name='Nombre', unique=True)
 
     def __str__(self):
-        return self.name
+        return 'Nombre: {}'.format(self.name)
 
     class Meta:
         verbose_name = 'Categoria'
         verbose_name_plural = 'Categorias'
-        db_table = 'categoria'
         ordering = ['id']
 
 
-class Employee(models.Model):
-    categ = models.ManyToManyField(Category)
-    type = models.ForeignKey(Type, on_delete=models.CASCADE)
+class Product(models.Model):
+    name = models.CharField(max_length=150, verbose_name='Nombre', unique=True)
+    cate = models.ForeignKey(Category, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='product/%Y/%m/%d', null=True, blank=True)
+    pvp = models.DecimalField(default=0.00, max_digits=9, decimal_places=2)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Producto'
+        verbose_name_plural = 'Productos'
+        ordering = ['id']
+
+
+class Client(models.Model):
     names = models.CharField(max_length=150, verbose_name='Nombres')
-    cui = models.CharField(max_length=13, unique=True, verbose_name='CUI')
-    date_joined = models.DateField(default=timezone.now, verbose_name='Fecha de registro')
-    date_creation = models.DateField(auto_now=True)
-    date_update = models.DateField(auto_now_add=True)
-    age = models.PositiveIntegerField(default=0)
-    salary = models.DecimalField(default=0.00, max_digits=9, decimal_places=2)
-    state = models.BooleanField(default=True)
-    gender = models.CharField(max_length=50, blank=True)
-    avatar = models.ImageField(upload_to='avatar/%Y/%m/%d', blank=True)
-    cvitae = models.FileField(upload_to='cvitae/%Y/%m/%d', blank=True)
+    surnames = models.CharField(max_length=150, verbose_name='Apellidos')
+    dni = models.CharField(max_length=10, unique=True, verbose_name='Dni')
+    birthday = models.DateField(default=datetime.now, verbose_name='Fecha de nacimiento')
+    address = models.CharField(max_length=150, null=True, blank=True, verbose_name='Dirección')
+    sexo = models.CharField(max_length=10, choices=gender_choices, default='male', verbose_name='Sexo')
 
     def __str__(self):
         return self.names
 
     class Meta:
-        verbose_name = 'Empleado'
-        verbose_name_plural = 'Empleados'
-        db_table = 'empleado'
+        verbose_name = 'Cliente'
+        verbose_name_plural = 'Clientes'
         ordering = ['id']
